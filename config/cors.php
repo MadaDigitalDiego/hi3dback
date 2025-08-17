2010a59dd782feccee5a4ae54af0504831bd4489
rickytreha <rickytreha@gmail.com>
2025-08-17 17:46:51 +0300

    correction dev2

--- config/cors.php
+++ config/cors.php
@@ -9,9 +9,9 @@ if (env('APP_ENV') === 'production') {
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://dev.hi-3d.com'],
+    'allowed_origins' => ['https://dev2.hi-3d.com'],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
-    
+
 ];
 

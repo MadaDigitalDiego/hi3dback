320462d54c7599b2c94c8c125b996288c68e5b17
rickytreha <rickytreha@gmail.com>
2025-08-17 17:31:16 +0300

    test corse

--- config/cors.php
+++ config/cors.php
@@ -7,40 +7,11 @@ if (env('APP_ENV') === 'production') {
 }
 
 return [
-
-/*
-|--------------------------------------------------------------------------
-| Cross-Origin Resource Sharing (CORS) Configuration
-|--------------------------------------------------------------------------
-|
-| Here you may configure your settings for cross-origin resource sharing
-| or "CORS". This determines what cross-origin operations may execute
-| in web browsers. You are free to adjust these settings as needed.
-|
-| To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-|
-*/
-
-'paths' => ['*'],
-
-'allowed_methods' => ['*'],
-
-'allowed_origins' => [
-    env('FRONTEND_URL', 'http://localhost:3000'),
-    env('APP_URL', 'http://localhost:8000'),
-    'http://localhost:3001',
-    'http://127.0.0.1:3000',
-    'https://dev2.hi-3d.com/',
-],
-
-'allowed_origins_patterns' => [],
-
-'allowed_headers' => ['*'],
-
-'exposed_headers' => [],
-
-'max_age' => 0,
-
-'supports_credentials' => false,
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['https://dev.hi-3d.com'],
+    'allowed_headers' => ['*'],
+    'supports_credentials' => true,
+    
 ];
+

cdb9dc3268318d5f122c55ae2a4c1a9c90d9a239
rickytreha <rickytreha@gmail.com>
2025-08-18 21:40:14 +0300

    correction cors

--- config/cors.php
+++ config/cors.php
@@ -9,9 +9,12 @@ if (env('APP_ENV') === 'production') {
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://dev2.hi-3d.com'],
+    'allowed_origins' => [
+        'https://dev2.hi-3d.com',
+    ],
     'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
     'supports_credentials' => true,
-
 ];
 
